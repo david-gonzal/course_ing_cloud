efc9f95b7d0305994ed67176b9e4ff9034f463ca
JulianGigena <juliangigena@MAC-68562.local>
2026-08-12 20:06:59 -0300

    add yml actions 7

--- modulos/extras/repaso3/main.tf
+++ modulos/extras/repaso3/main.tf
@@ -1,29 +1,29 @@
 data "aws_caller_identity" "current" {}
 
 #1. Bucket s3
-resource "aws_s3_bucket" "auditoria_ingesta"{
-    bucket = "auditoria-${data.aws_caller_identity.current.account_id}"
-    force_destroy = true
+resource "aws_s3_bucket" "auditoria_ingesta" {
+  bucket        = "auditoria-${data.aws_caller_identity.current.account_id}"
+  force_destroy = true
 }
 
 #2. Rol IAM
-resource "aws_iam_role" "lambda_auditoria_role"{
-    name = "role_procesador_auditoria_lambda"
+resource "aws_iam_role" "lambda_auditoria_role" {
+  name = "role_procesador_auditoria_lambda"
 
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [{
-            Action = "sts:AssumeRole"
-            Effect = "Allow"
-            Principal = { Service = "lambda.amazonaws.com"}
-        }]
-    })
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
 }
 
 #Permisos de logs CloudWatch
-resource "aws_iam_role_policy_attachment" "lambda_logs_policy"{
-    role = aws_iam_role.lambda_auditoria_role.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_iam_role_policy_attachment" "lambda_logs_policy" {
+  role       = aws_iam_role.lambda_auditoria_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 #Empaquetado automatico de python
@@ -57,18 +57,18 @@ EOF
 }
 
 #3 Funtion lambda
-resource "aws_lambda_function" "log_auditoria_processor"{
-    function_name = "analizador_registros_transacciones"
-    role = aws_iam_role.lambda_auditoria_role.arn
-    handler = "app.handler"
-    filename = data.archive_file.lambda_zip.output_path
-    source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-    runtime = "python3.11"
+resource "aws_lambda_function" "log_auditoria_processor" {
+  function_name    = "analizador_registros_transacciones"
+  role             = aws_iam_role.lambda_auditoria_role.arn
+  handler          = "app.handler"
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  runtime          = "python3.11"
 
-    environment{
-        variables = {
-            AUDI_BUCKET_TARGET = aws_s3_bucket.auditoria_ingesta.id
-            ENVIRONMENT = "evaluacion-dev"
-        }
+  environment {
+    variables = {
+      AUDI_BUCKET_TARGET = aws_s3_bucket.auditoria_ingesta.id
+      ENVIRONMENT        = "evaluacion-dev"
     }
+  }
 }
